3777efc74e15067b4cab806b0e90386b8b6c8ba2
Andy Wong <andy.wongtszkiu@gmail.com>
2024-09-19 00:14:58 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "https://data.etabus.gov.hk",
+        target: process.env.VITE_API_URL || "https://data.etabus.gov.hk",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
